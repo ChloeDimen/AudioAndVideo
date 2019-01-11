b8346b3e3fda629ec646a6813f902ad7f4022e68
ChloeDimen <1351703151@qq.com>
2019-01-11 15:27:48 +0800

    音频开发第二章：3种音频采集和播放

--- app/src/main/java/com/dimen/audioandvideo/Record/MediaRecorderManger.java
+++ app/src/main/java/com/dimen/audioandvideo/Record/MediaRecorderManger.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 
 public class MediaRecorderManger {
     private boolean isRecord = false;
-
     private MediaRecorder mMediaRecorder;
     private MediaRecorderManger(){
     }
